a06b2dcb317f92f10a5306e48cd3b0f5b99ff1fe
Leonardo <fernandes.leo@live.com>
2024-10-05 19:20:45 -0300

    Initial Commit

---
+++ WayAirlinesTests/WayAirlinesTests.swift
@@ -0,0 +1,17 @@
+//
+//  WayAirlinesTests.swift
+//  WayAirlinesTests
+//
+//  Created by Leonardo Fernandes on 05/10/24.
+//
+
+import Testing
+@testable import WayAirlines
+
+struct WayAirlinesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
